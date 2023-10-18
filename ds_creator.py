a41712a64e2ef76c5623e707cd2186608f21ff83
quercusilvam <in.silvam.ligna.ne.feras@gmail.com>
2023-10-18 08:18:30 +0200

    Refactor splitting audio file into chunks

--- ds_creator.py
+++ ds_creator.py
@@ -8,7 +8,8 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
 MIN_SILENCE_LEN = 500
-SILENCE_THRESH = -45
+SILENCE_THRESHOLD = -50
+MAX_CHUNK_LENGTH = 30 * 1000
 WHISPER_SAMPLING = 16000
 
 files = ['test_1']
@@ -26,28 +27,38 @@ def load_audio_file(path, frame_rate=WHISPER_SAMPLING):
     return audio_file
 
 
-for f in files:
-    print(f'Processing file {f}{FILE_FORMAT}')
-    song = load_audio_file(INPUT_DIR + f + FILE_FORMAT)
+def generate_audio_chunks(audio_file, min_silence_len=MIN_SILENCE_LEN,
+                          silence_threshold=SILENCE_THRESHOLD,
+                          max_chunk_length=MAX_CHUNK_LENGTH):
+    """Generate audio chunks that are split on silence and has max length.
 
-    print(f'  Split on silence')
-    chunks = split_on_silence(
-        song,
-        min_silence_len=MIN_SILENCE_LEN,
-        silence_thresh=SILENCE_THRESH,
+    Find silence spots in audio file (with min_silence_len in ms) and use them as breaking points. After that find
+    the largest possible chunks that start/ends with silence spot but not exceed max_chunk_length. This way we get
+    chunks as close as possible to chunks using in speach recognition whisper tool, and we are sure audio is not
+    split in the middle of spoken word
+    """
+    silent_spots = split_on_silence(
+        audio_file,
+        min_silence_len=min_silence_len,
+        silence_thresh=silence_threshold,
         keep_silence=True,
         seek_step=50
     )
-
-    # now recombine the chunks so that the parts are at max 30 sec long
-    print(f'  Create chunks as long as possible')
-    max_length = 30 * 1000
-    output_chunks = [chunks[0]]
-    for chunk in chunks[1:]:
-        if len(output_chunks[-1]) + len(chunk) < max_length:
-            output_chunks[-1] += chunk
+    output_chunks = [silent_spots[0]]
+    for ss in silent_spots[1:]:
+        if len(output_chunks[-1]) + len(ss) < max_chunk_length:
+            output_chunks[-1] += ss
         else:
-            output_chunks.append(chunk)
+            output_chunks.append(ss)
+    return output_chunks
+
+
+for f in files:
+    print(f'Processing file {f}{FILE_FORMAT}')
+    audio = load_audio_file(INPUT_DIR + f + FILE_FORMAT)
+
+    print(f'  Create chunks as long as possible')
+    audio_chunks = generate_audio_chunks(audio)
 
     chunk_file_dir = os.path.join(CHUNK_DIR, f)
     ds_file_dir = os.path.join(DS_DIR, f)
@@ -66,10 +77,10 @@ for f in files:
     output_chunks_len = []
     output_ds = None
 
-    for i, chunk in enumerate(output_chunks):
+    for i, chunk in enumerate(audio_chunks):
         output_chunks_fn.append(chunk_file_dir + '/chunk{0}{1}'.format(i, FILE_FORMAT))
         output_chunks_len.append(len(chunk))
-        print('  Exporting chunk{0}. Len {1}'.format(output_chunks_fn[-1], output_chunks_len[-1]))
+        print('  Exporting {0}. Len {1}'.format(output_chunks_fn[-1], output_chunks_len[-1]))
         chunk.export(
             output_chunks_fn[-1],
             format='mp3'
