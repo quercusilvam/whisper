1b6cfd12ec099ff8f3175b0b8763ffb422fe9278
quercusilvam <in.silvam.ligna.ne.feras@gmail.com>
2023-10-23 12:21:38 +0200

    Add create of test DS to ds_creator - part 2

--- ds_creator.py
+++ ds_creator.py
@@ -34,37 +34,42 @@ def main():
 
 def create_and_verify_ds(filename):
     """Process audio files, create DS based on them and verify it integrity."""
-    process_audio_file(filename)
-    verify_ds(filename)
+    if is_create_ds_from_input:
+        print(f'Processing file {filename}.{AUDIO_FILE_FORMAT}')
+        process_audio_file(filename)
+        verify_ds(os.path.join(OUTPUT_DIR, CHUNK_DIR, filename), os.path.join(OUTPUT_DIR, DS_DIR, filename), filename)
+    else:
+        print(f'Processing of file {filename} skipped')
 
 
 def create_and_verify_test_ds(dirname):
     """Create test DS based on audio_chunks in 'test' folder and transcription for each chunk."""
-    create_test_ds(dirname)
+    if is_create_test_ds:
+        print(f'Create test DS for folder {dirname}')
+        process_test_dir(dirname)
+        verify_ds(os.path.join(TEST_DIR, CHUNK_DIR, dirname), os.path.join(TEST_DIR, DS_DIR, dirname), dirname)
+    else:
+        print(f'Create of test DS for {dirname} skipped')
 
 
 def process_audio_file(audio_file):
     """Convert audio files to chunks and dataset"""
-    if is_create_ds_from_input:
-        print(f'Processing file {audio_file}.{AUDIO_FILE_FORMAT}')
-        audio = load_audio_file(f'{INPUT_DIR}{audio_file}.{AUDIO_FILE_FORMAT}')
+    audio = load_audio_file(f'{INPUT_DIR}{audio_file}.{AUDIO_FILE_FORMAT}')
 
-        print(f'  Create chunks as long as possible')
-        audio_chunks = generate_audio_chunks(audio)
+    print(f'  Create chunks as long as possible')
+    audio_chunks = generate_audio_chunks(audio)
 
-        print(f'  Init output dir')
-        chunk_file_dir = os.path.join(OUTPUT_DIR, CHUNK_DIR, audio_file)
-        ds_file_dir = os.path.join(OUTPUT_DIR, DS_DIR, audio_file)
-        init_output_dir(chunk_file_dir)
-        init_output_dir(ds_file_dir)
+    print(f'  Init output dir')
+    chunk_file_dir = os.path.join(OUTPUT_DIR, CHUNK_DIR, audio_file)
+    ds_file_dir = os.path.join(OUTPUT_DIR, DS_DIR, audio_file)
+    init_output_dir(chunk_file_dir)
+    init_output_dir(ds_file_dir)
 
-        # Process each chunk with your parameters
-        print(f'  Save chunks')
-        chunks_dict_list = save_audio_chunks(audio_chunks, chunk_file_dir)
-        print(f'  Save dataset')
-        create_and_save_ds(chunks_dict_list, ds_file_dir)
-    else:
-        print(f'Processing of file {audio_file} skipped')
+    # Process each chunk with your parameters
+    print(f'  Save chunks')
+    chunks_dict_list = save_audio_chunks(audio_chunks, chunk_file_dir)
+    print(f'  Save dataset')
+    create_and_save_ds(chunks_dict_list, ds_file_dir)
 
 
 def load_audio_file(path, frame_rate=WHISPER_SAMPLING):
@@ -115,7 +120,6 @@ def save_audio_chunks(chunks, chunk_dir_path, file_format=AUDIO_FILE_FORMAT):
     Save audio chunks as audio file in given chunk_dir_path with given file_format.
     Return list of dictionaries with details about saved chunks
     """
-
     output_dict = []
     for i, chunk in enumerate(chunks):
         output_chunk_fn = chunk_dir_path + '/chunk{:04d}.{}'.format(i, file_format)
@@ -143,29 +147,46 @@ def create_and_save_ds(chunks_dict_list, ds_file_dir):
     output_ds.save_to_disk(ds_file_dir)
 
 
-def verify_ds(dirname):
+def verify_ds(chunks_dir, ds_dir, dirname):
     """Verify created dataset"""
     if is_verify_created_ds:
         print(f'  Read dataset {dirname}')
-        ds = read_dataset_from_dir(os.path.join(OUTPUT_DIR, DS_DIR, dirname))
-        chunks = read_audio_chunks_from_dir(os.path.join(OUTPUT_DIR, CHUNK_DIR, dirname))
+        ds = read_dataset_from_dir(ds_dir)
+        chunks = read_audio_chunks_from_dir(chunks_dir)
         print(f'  Verify dataset {dirname}')
         compare_dataset_with_audio_chunks(ds, chunks)
     else:
         print(f'  Verification of {dirname} skipped')
 
 
-def create_test_ds(dirname):
+def process_test_dir(dirname):
+    """Process test dirname. Create dataset with audio samples and transcription."""
+    print(f'  Init output dir')
+    ds_file_dir = os.path.join(TEST_DIR, DS_DIR, dirname)
+    init_output_dir(ds_file_dir)
+    print(f'  Save dataset')
+    create_and_save_test_ds(os.path.join(TEST_DIR, CHUNK_DIR, dirname), ds_file_dir)
+
+
+def create_and_save_test_ds(chunk_dir, ds_file_dir):
     """Create test DS based on audio_chunks with transcription."""
-    if is_create_test_ds:
-        print(f'  Create test DS for folder {dirname}')
-        chunk_dir = os.path.join(TEST_DIR, CHUNK_DIR, dirname)
-        print(f'  Read chunks from {chunk_dir}')
-        chunks = read_audio_chunks_from_dir(chunk_dir)
-        print(f'  Read transcription from {chunk_dir}')
-        transcriptions = read_transcriptions_from_dir(chunk_dir)
+    print(f'  Read chunks from {chunk_dir}')
+    chunks = read_audio_chunks_from_dir(chunk_dir)
+    print(f'  Read transcription from {chunk_dir}')
+    transcriptions = read_transcriptions_from_dir(chunk_dir)
+    if len(chunks) == len(transcriptions):
+        output_ds = None
+        for i, c in enumerate(chunks):
+            chunk = {**c, **transcriptions[i]}
+            print(chunk)
+            if i == 0:
+                output_ds = Dataset.from_dict(chunk).cast_column('audio', Audio())
+            else:
+                ds = Dataset.from_dict(chunk).cast_column('audio', Audio())
+                output_ds = output_ds.add_item(ds[0])
+        output_ds.save_to_disk(ds_file_dir)
     else:
-        print(f' Create of test DS for {dirname} skipped')
+        print(f'  [ERROR] Length of audio chunks in {chunk_dir} is different than transcriptions number')
 
 
 def read_dataset_from_dir(ds_dir):
@@ -179,9 +200,10 @@ def read_audio_chunks_from_dir(audio_chunks_dir):
     output_dict = []
     files = pathlib.Path(audio_chunks_dir).glob(f'*.{AUDIO_FILE_FORMAT}')
     for f in list(files):
+        f = f.__str__()
         # print(f'  Found audio chunk {f}')
         audio_file = AudioSegment.from_mp3(f)
-        d = {'file': f, 'len': len(audio_file)}
+        d = {'file': [f], 'audio': [f], 'len': [len(audio_file)]}
         output_dict.append(d)
     return output_dict
 
@@ -192,8 +214,9 @@ def read_transcriptions_from_dir(transcription_chunks_dir):
     files = pathlib.Path(transcription_chunks_dir).glob(f'*.{TRANSCRIPTION_FILE_FORMAT}')
     for f in list(files):
         # print(f'  Found transcription chunk {f}')
-        transcription = '#TODO'
-        d = {'transcription': [transcription]}
+        with open(f, 'r') as file:
+            transcription = file.readlines()
+        d = {'transcription': transcription}
         output_dict.append(d)
     return output_dict
 
@@ -201,14 +224,19 @@ def read_transcriptions_from_dir(transcription_chunks_dir):
 def compare_dataset_with_audio_chunks(ds, audio_chunks):
     """Compare if filename and length of dataset is the same as read from audio chunks."""
     for i, d in enumerate(ds):
-        if d['file'] != audio_chunks[i]['file']:
+        ac_file = audio_chunks[i]['file'][0]
+        d_file = d['file']
+        if d_file != ac_file:
             print('  Checking ds {}'.format(d))
             print('  With audio_chunk {}'.format(audio_chunks[i]))
-            print('  [ERROR] - dataset file {0} is not equal audio chunk file {1}'.format(d['file'], audio_chunks[i]['file']))
-        if d['len'] != audio_chunks[i]['len']:
+            print('  [ERROR] - dataset file {0} is not equal audio chunk file {1}'.format(d_file, ac_file))
+
+        ac_len = audio_chunks[i]['len'][0]
+        d_len = d['len']
+        if d_len != ac_len:
             print('  Checking ds {}'.format(d))
             print('  With audio_chunk {}'.format(audio_chunks[i]))
-            print('  [ERROR] - dataset len {0} is not equal audio chunk len {1}'.format(d['len'], audio_chunks[i]['len']))
+            print('  [ERROR] - dataset len {0} is not equal audio chunk len {1}'.format(d_len, ac_len))
     print('[OK]')
 
 
