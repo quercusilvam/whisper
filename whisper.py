28ba1a9e31b7f8d328bb40c46914c234c0153272
quercusilvam <in.silvam.ligna.ne.feras@gmail.com>
2023-10-24 22:33:27 +0200

    Add calculate_wer function to whisper.py

--- whisper.py
+++ whisper.py
@@ -6,7 +6,6 @@ import gc
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from datasets import Dataset, Audio
 
-
 INPUT_DIR = 'audio_chunks/'
 TEST_DIR = 'test/'
 CHUNKS_DIR = 'chunks/'
@@ -37,7 +36,23 @@ def process_sample(sample, processor, model):
     return text[0]
 
 
-exit_values=[]
+def calculate_wer(test_processor, test_model, test_ds):
+    """Calculate WER for given test_processor/test_model based on test_ds."""
+    total_wer = 0
+    average_wer = 0
+    for i, d in enumerate(test_ds):
+        l = d['len']
+        transcription = d['transcription']
+        prediction_text = process_sample(d['audio'], test_processor, test_model)
+        wer = 100 * metric.compute(predictions=[prediction_text], references=[transcription])
+        total_wer += wer
+        average_wer = total_wer / (i + 1)
+        t = 'Chunk {}, current WER={:.2f}; total WER={:.2f}; average WER={:.2f}'
+        print(t.format(i + 1, wer, total_wer, average_wer))
+    return dict({'total_wer': total_wer, 'average_wer': average_wer})
+
+
+exit_values = []
 for i, m in enumerate(MODELS):
     print(f'Checking model {m}')
 
@@ -56,22 +71,25 @@ for i, m in enumerate(MODELS):
         print(f'Read dataset {dir_name.name}')
         ds = Dataset.load_from_disk(dir_name.path)
 
-        total_length = 0
-        total_wer = 0
-        average_wer = 0
-        for i, d in enumerate(ds):
-            l = d['len']
-            transcription = d['transcription']
-            print(f'{microseconds_to_audio_timestamp(total_length)} -> {microseconds_to_audio_timestamp(total_length + l)}')
-            total_length += l
-            prediction_text = process_sample(d['audio'], processor, model)
-            wer = 100 * metric.compute(predictions=[prediction_text], references=[transcription])
-            total_wer += wer
-            average_wer = total_wer/(i+1)
-            print(prediction_text)
-            print(transcription)
-            print(f'current WER={wer}; total WER={total_wer}; average WER={average_wer}')
+        calculated_wer = calculate_wer(processor, model, ds)
 
-        exit_values.append(f'Model {m}. File {dir_name.name}. Total WER={total_wer}; average WER={average_wer}')
+        t = 'Model {}. File {}. Total WER={:.2f}; average WER={:.2f}'
+        exit_values.append(t.format(m, dir_name.name, calculated_wer['total_wer'], calculated_wer['average_wer']))
+
+        # total_length = 0
+        # total_wer = 0
+        # average_wer = 0
+        # for i, d in enumerate(ds):
+        #     l = d['len']
+        #     transcription = d['transcription']
+        #     print(f'{microseconds_to_audio_timestamp(total_length)} -> {microseconds_to_audio_timestamp(total_length + l)}')
+        #     total_length += l
+        #     prediction_text = process_sample(d['audio'], processor, model)
+        #     wer = 100 * metric.compute(predictions=[prediction_text], references=[transcription])
+        #     total_wer += wer
+        #     average_wer = total_wer/(i+1)
+        #     print(prediction_text)
+        #     print(transcription)
+        #     print(f'current WER={wer}; total WER={total_wer}; average WER={average_wer}')
 
 print(exit_values)
