53971ced854eca13e317dc031de8de44d82fc78f
quercusilvam <in.silvam.ligna.ne.feras@gmail.com>
2023-10-28 21:12:56 +0200

    Add support of Nvidia CUDA

--- whisper.py
+++ whisper.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 import evaluate
-import gc
+import torch
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from datasets import Dataset, Audio
 
@@ -10,14 +10,28 @@ INPUT_DIR = 'audio_chunks/'
 TEST_DIR = 'test/'
 CHUNKS_DIR = 'chunks/'
 DS_DIR = 'ds/'
-MODEL = 'openai/whisper-small' # 'bardsai/whisper-small-pl']
+MODEL = 'openai/whisper-small'
+
+parser = argparse.ArgumentParser(description='Transcript input dataset with audio data to text. Also benchmark models')
+parser.add_argument('-b', '--benchmark', action='store_true',
+                    help='If set the script will only benchmark model(s) based on test dataset(s)')
+args = parser.parse_args()
+
+is_only_benchmark = args.benchmark
 
 
 def main():
     """Main function."""
-    processor = WhisperProcessor.from_pretrained(MODEL, language='pl', task='transcribe')
-    model = WhisperForConditionalGeneration.from_pretrained(MODEL)
+    print(f'Starting loading model {MODEL}')
+    if torch.cuda.is_available():
+        processor = WhisperProcessor.from_pretrained(MODEL, language='pl', task='transcribe')
+        model = WhisperForConditionalGeneration.from_pretrained(MODEL).to('cuda')
+    else:
+        processor = WhisperProcessor.from_pretrained(MODEL, language='pl', task='transcribe')
+        model = WhisperForConditionalGeneration.from_pretrained(MODEL)
+
     model.config.forced_decoder_ids = None
+    print('Ended loading model')
 
     if is_only_benchmark:
         benchmark_model(processor, model)
@@ -37,26 +51,24 @@ def benchmark_model(processor, model):
 
         calculated_wer = calculate_wer(processor, model, ds, metric)
 
-        t = 'Model {}. File {}. Total WER={:.2f}; average WER={:.2f}'
-        exit_values.append(t.format(MODEL, dir_name.name, calculated_wer['total_wer'], calculated_wer['average_wer']))
+        t = 'Model {}. File {}. WER={:.2f}'
+        exit_values.append(t.format(MODEL, dir_name.name, calculated_wer['wer']))
 
     print(exit_values)
 
 
 def calculate_wer(test_processor, test_model, test_ds, metric):
     """Calculate WER for given test_processor/test_model based on test_ds."""
-    total_wer = 0
-    average_wer = 0
-    for i, d in enumerate(test_ds):
-        l = d['len']
-        transcription = d['transcription']
-        prediction_text = process_sample(d['audio'], test_processor, test_model)
-        wer = 100 * metric.compute(predictions=[prediction_text], references=[transcription])
-        total_wer += wer
-        average_wer = total_wer / (i + 1)
-        t = 'Chunk {}, current WER={:.2f}; total WER={:.2f}; average WER={:.2f}'
-        print(t.format(i + 1, wer, total_wer, average_wer))
-    return dict({'total_wer': total_wer, 'average_wer': average_wer})
+    transcription = []
+    prediction_text = []
+    for d in test_ds:
+        chunk = d['file']
+        print(f'Read chunk {chunk}')
+        transcription.append(d['transcription'])
+        prediction_text.append(process_sample(d['audio'], test_processor, test_model))
+    wer = 100 * metric.compute(predictions=prediction_text, references=transcription)
+    print('WER={:.2f}'.format(wer))
+    return dict({'wer': wer})
 
 
 def generate_transcript(processor, model):
@@ -91,18 +103,13 @@ def process_sample(sample, processor, model):
     input_features = processor(sample['array'], sampling_rate=sample['sampling_rate'],
                                return_tensors='pt').input_features
     forced_decoder_ids = processor.get_decoder_prompt_ids(language='pl', task='transcribe')
+    if torch.cuda.is_available():
+        input_features = input_features.to('cuda')
 
     predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
     text = processor.batch_decode(predicted_ids, skip_special_tokens=True)
 
     return text[0]
 
-
-parser = argparse.ArgumentParser(description='Transcript input dataset with audio data to text. Also benchmark models')
-parser.add_argument('-b', '--benchmark', action='store_true',
-                    help='If set the script will only benchmark model(s) based on test dataset(s)')
-args = parser.parse_args()
-
-is_only_benchmark = args.benchmark
 
 main()
